68ffc5a568bf13e66d324aba5794237505b73e1e
Alex Muirhead <a.muirhead@uq.net.au>
2025-04-16 17:37:35 +1000

    Add some more grid parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::{Context, Result, bail};
 use clap::Parser;
 use nalgebra::Vector3;
 use std::{
     fs::{self, File},
-    io::{self, Error, ErrorKind},
+    io::Read,
 };
 
 #[derive(Parser, Debug)]
@@ -26,51 +27,53 @@ struct StructuredGrid {
 //     let shape = (0, 0, 0);
 // }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let args = Args::parse();
 
     println!("Args: {:?}", args);
 
     let sim_directory = fs::read_dir("./".to_owned() + &args.dir)?
         .flatten() // Remove all errors
-        .find(|e| e.file_name() == "lmrsim" && e.path().is_dir());
-
-    let Some(sim_directory) = sim_directory else {
-        return Err(Error::new(
-            ErrorKind::Other,
-            "Directory does not contain a valid lmrsim folder",
-        ));
-    };
+        .find(|e| e.file_name() == "lmrsim" && e.path().is_dir())
+        .context("Directory does not contain a valid lmrsim folder")?;
 
     let grid_directory = sim_directory
         .path()
         .read_dir()?
         .flatten()
-        .find(|e| e.file_name() == "grid" && e.path().is_dir());
-
-    let Some(grid_directory) = grid_directory else {
-        return Err(Error::new(
-            ErrorKind::Other,
-            "lmrsim directory does not contain grid folder",
-        ));
-    };
+        .find(|e| e.file_name() == "grid" && e.path().is_dir())
+        .context("lmrsim directory does not contain grid folder")?;
 
     let snapshots_directory = sim_directory
         .path()
         .read_dir()?
         .flatten()
-        .find(|e| e.file_name() == "snapshots" && e.path().is_dir());
-
-    let Some(snapshots_directory) = snapshots_directory else {
-        return Err(Error::new(
-            ErrorKind::Other,
-            "lmrsim directory does not contain snapshots folder",
-        ));
-    };
+        .find(|e| e.file_name() == "snapshots" && e.path().is_dir())
+        .context("lmrsim directory does not contain snapshots folder")?;
 
     println!("Dir: {:?}", sim_directory);
     println!("Grid Dir: {:?}", grid_directory);
     println!("Snapshots Dir: {:?}", snapshots_directory);
+
+    let grid_metadata_path = grid_directory.path().join("grid.metadata");
+    println!(
+        "Does the grid-metadata file exist? {:?}",
+        grid_metadata_path.try_exists()
+    );
+    let mut grid_metadata_file = File::open(grid_metadata_path)?;
+    let mut grid_metadata = String::new();
+    grid_metadata_file.read_to_string(&mut grid_metadata)?;
+
+    let grid_metadata = json::parse(&grid_metadata).context("Could not parse grid metadata")?;
+    println!("Grid metadata: {:?}", grid_metadata);
+
+    let Some(num_grids) = grid_metadata["ngrids"].as_u8() else {
+        bail!("Number of grids is poorly formed");
+    };
+
+    println!("Number of grids: {}", num_grids);
+
+    for grid_num in 0..num_grids {}
 
     Ok(())
 }
